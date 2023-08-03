7e6552cb3ef035e8c28355a4bff1e62d071be372
Ali <ali.salloum.gh@gmail.com>
2023-08-03 16:08:54 +0300

    added database and logging

--- bot.py
+++ bot.py
@@ -5,6 +5,7 @@ import telebot
 from threading import Thread
 from time import sleep
 import requests
+from pymongo import MongoClient
 
 load_dotenv()
 
@@ -13,13 +14,18 @@ URL = os.getenv('URL')
 
 bot = telebot.TeleBot(API_KEY)
 
-# Dictionary to keep track of users who activated the newsletter
-subscribed_users = {}
+# Set up the connection to MongoDB
+MONGO_URI = os.getenv('MONGO_URI')
+mongo_client = MongoClient(MONGO_URI)
+db = mongo_client.get_database("WSI")
+subscribed_users_collection = db.get_collection("subscribers")
+
 
 def schedule_checker():
     while True:
         schedule.run_pending()
         sleep(60)
+
 
 def get_report():
     report = ""
@@ -41,47 +47,72 @@ def get_report():
 
     return report, error
 
+
 def send_daily_report():
     report, error = get_report()
-
+    print('sending daily report initialized')
+    print('error value: ',error)
+    subscribed_users = subscribed_users_collection.find({"subscribed": True})
+    counter = 0
     for user_id in subscribed_users:
         if not error:
             bot.send_message(user_id, report)
         else:
-            bot.send_message(user_id, "Sorry, an error occurred when calling the server. We can't show the daily report today :(")
+            bot.send_message(
+                user_id, "Sorry, an error occurred when calling the server. We can't show the daily report today :(")
+        counter = counter + 1
+        if counter % 5 == 0:
+            sleep(5)
+    print('done sending report')
 
 @bot.message_handler(commands=['start'])
 def start(message):
     user_id = message.from_user.id
-    subscribed_users[user_id] = True
-    bot.send_message(user_id, "You have subscribed to the daily report! Send /stop to unsubscibe.")
+    username = 'unknown'
+    if message.from_user.username:
+        username = message.from_user.username
+    print(f'/start message received from user_id: {user_id} username: {username}')
+    subscribed_users_collection.update_one(
+        {"user_id": user_id, "username": username}, {"$set": {"subscribed": True}}, upsert=True)
+    bot.send_message(
+        user_id, "You have subscribed to the daily report! Send /stop to unsubscribe.")
+    print(f'/start successfully handled user_id: {user_id} username: {username}')
 
 @bot.message_handler(commands=['send'])
 def send(message):
     user_id = message.from_user.id
-    bot.send_message(user_id, "Loading the report...")
+    username = 'unknown'
+    if message.from_user.username:
+        username = message.from_user.username
+    print(f'/send message received from user_id: {user_id} username: {username}')
+    bot.send_message(
+        user_id, "Loading the report. This can take a few minutes...")
     report, error = get_report()
     if error:
-        bot.send_message(user_id, "Sorry, an error occurred when loading the report")
+        bot.send_message(
+            user_id, "Sorry, an error occurred when loading the report")
     else:
         bot.send_message(user_id, report)
+    print(f'/send message handled for user_id: {user_id} username: {username}')
 
 @bot.message_handler(commands=['stop'])
 def stop(message):
     user_id = message.from_user.id
-    if user_id in subscribed_users:
-        subscribed_users.pop(user_id)
-        bot.send_message(user_id, "You have unsubscribed from the daily report.")
-    else:
-        bot.send_message(user_id, "You are not subscribed to the daily report.")
+    username = 'unknown'
+    if message.from_user.username:
+        username = message.from_user.username
+    print(f'/stop message received from user_id: {user_id} username: {username}')
+    subscribed_users_collection.delete_one({"user_id": user_id})
+    bot.send_message(user_id, "You have unsubscribed from the daily report. Send /start to subscribe again.")
+    print(f'/stop message handled for user_id: {user_id} username: {username}')
 
 if __name__ == "__main__":
     # Create the job in schedule.
-    schedule.every().day.at("00:00").do(send_daily_report)
+    schedule.every().day.at("00:00").do(send_daily_report)  # 09:00 for MSK
 
     # Spin up a thread to run the schedule check so it doesn't block your bot.
     # This will take the function schedule_checker which will check every minute
     # to see if the scheduled job needs to be ran.
-    Thread(target=schedule_checker).start() 
+    Thread(target=schedule_checker).start()
 
     bot.infinity_polling()
